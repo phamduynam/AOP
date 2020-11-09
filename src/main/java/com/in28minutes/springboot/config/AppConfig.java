3e981dd2fff376b94a28f621942e52a42179cc9d
phamduynam <nambh1998xx@gmail.com>
2020-11-09 14:49:37 +0700

    update ask 1

--- src/main/java/com/in28minutes/springboot/config/AppConfig.java
+++ src/main/java/com/in28minutes/springboot/config/AppConfig.java
@@ -1,18 +1,24 @@
 package com.in28minutes.springboot.config;
 
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
 
 
 @Configuration
@@ -20,38 +26,57 @@ import javax.sql.DataSource;
 @EnableJpaRepositories(
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "customersTransactionManager",
-        basePackages = "com.in28minutes.springboot")
-// Nếu không có thì tự động lấy ở trong resource.
+        basePackages = "com.in28minutes.springboot.repository")
 @PropertySource("classpath:application.properties")
-@ComponentScan
+@ComponentScan({"com.in28minutes.springboot.model"})
 public class AppConfig {
 
-    @Primary // Ưu tiên lựa chọn nếu có cùng contex
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource") // lấy các config có tiền tố spring.datasource cấu hình
-    public DataSource getDataSource() {
-        return DataSourceBuilder.create().build();
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername( environment.getProperty("spring.datasource.username") );
+        dataSource.setPassword( environment.getProperty("spring.datasource.password"));
+        return dataSource;
     }
 
-    @Primary
-    @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("dataSource") DataSource dataSource
-    ) {
-        return builder
-                .dataSource(dataSource)
-                .packages("com.in28minutes.springboot")
-                .persistenceUnit("model")
-                .build();
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.in28minutes.springboot.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
     }
 
-    @Primary
-    @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(
-            @Qualifier("entityManagerFactory") EntityManagerFactory
-                    entityManagerFactory
-    ) {
-        return new JpaTransactionManager(entityManagerFactory);
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return transactionManager;
+    }
+
+//    @Bean
+//    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+//        return new PersistenceExceptionTranslationPostProcessor();
+//    }
+
+    final Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
+        return properties;
     }
 }
